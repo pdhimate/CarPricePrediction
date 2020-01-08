25689370dc772fc8ac53481c398c9ac0cadbb4c6
Piyush Dhimate <bitartist@hotmail.com>
2020-01-08 20:14:50 +1100

    Add project files.

---
+++ CarPricePrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarPricePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
